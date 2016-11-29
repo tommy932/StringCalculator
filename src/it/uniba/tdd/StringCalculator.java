d260fa6cd28147ae0a1d2bf2ab9e8d72acdfec60
tommy932 <tommy.931@live.it>
2016-11-29 16:06:29 +0100

    Task 4 complete

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,38 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+
+		if (numbersStr.isEmpty()) {
+			return 0;
+		}
+
+		else if (numbersStr.length() == 1) {
+			return Integer.parseInt(numbersStr);
+		}
+
+		else {
+
+			char delimitator;
+			if (numbersStr.contains("//")) {
+				delimitator = numbersStr.charAt(2);
+				numbersStr = numbersStr.replace("//" + delimitator + "\n", "");
+				if (numbersStr.contains(delimitator + "\n") || numbersStr.contains("\n" + delimitator))
+					throw new StringCalculatorException("Format number error.");
+			} else {
+				delimitator = ',';
+				if (numbersStr.contains(",\n") || numbersStr.contains("\n,"))
+					throw new StringCalculatorException("Format number error.");
+			}
+			int total = 0;
+			String split[] = numbersStr.split("(" + delimitator + ")|(\\\n)");
+			for (String number : split) {
+				total += Integer.parseInt(number);
+			}
+			return total;
+		}
+
 	}
 }
